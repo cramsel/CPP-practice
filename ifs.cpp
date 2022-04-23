c91aaba4f68bd4d7be66df5dc11b775f9b50bf84
cramsel <luxicorde@gmail.com>
2022-04-22 19:24:14 -0500

    if/else pract 4-22-22-1

--- ifs.cpp
+++ ifs.cpp
@@ -1,16 +1,32 @@
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
-  int x;
-  cout << "Enter a number: ";
-  cin >> x;
-  // cout << "The number is " << x << endl;
-  if (x<5) {
-    cout << x << " is less than 5." << endl;
-  } else if (x==5) {
-    cout << x << " is 5" << endl;
-  } else {
-    cout << x << " is greater than 5" << endl;
-  }
-}
+// int main() {
+//   int x;
+//   cout << "Enter a number: ";
+//   cin >> x;
+//   // cout << "The number is " << x << endl;
+//   if (x<5) {
+//     cout << x << " is less than 5." << endl;
+//   } else if (x==5) {
+//     cout << x << " is 5" << endl;
+//   } else {
+//     cout << x << " is greater than 5" << endl;
+//   }
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int x;
+//   cout << "Enter a number: ";
+//   cin >> x;
+//   if (x<10) {
+//     cout << 0 << endl;
+//   } else if (x>=10) {
+//     cout << 1 << endl;
+//   }
+//   return 0;
+// }
