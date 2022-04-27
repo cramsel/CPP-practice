6e302b06a656e63d6290240f19bf1f4dc24f4796
cramsel <luxicorde@gmail.com>
2022-04-26 23:53:56 -0500

    Make array file for learning about cpp arrays

---
+++ arrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //arrays go data type, name of array, number of elements in array,
+  //elements within curly brackets {}
+  
+  // int ages[3] = {1,7,16};
+  // cout << ages;
+  // return 0;
+  //compiled as is, this spits out "0x7fffc7a3a1cc"
+
+  int ages[3] = {1,7,16};
+  cout << ages[2];
+  return 0;
+  //this works as it would in ruby, printing the third element
+}
